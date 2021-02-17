a79b6e2357e675bbacf905ab72334d01923cb489
wanderleimagri <wanderleimagri@gmail.com>
2021-02-17 11:07:47 -0300

    commit

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type Pessoa struct {
-	Id    int
-	Nome  string
-	Email string
+	Id    int    `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
 }
 
 var Pessoas []Pessoa = []Pessoa{
@@ -29,18 +30,30 @@ func rotaPrincipal(w http.ResponseWriter, r *http.Request) {
 }
 
 func listarPessoas(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Pessoas)
+}
+
+func cadastrarPessoa(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		return
+	}
 }
 
 func configurarRotas() {
 	http.HandleFunc("/", rotaPrincipal)
 	http.HandleFunc("/pessoas", listarPessoas)
+	http.HandleFunc("/pessoas", cadastrarPessoa)
 }
 
 func configurarServidor() {
 	configurarRotas()
+
 	fmt.Println("Servidor rodando em localhost:8888")
-	http.ListenAndServe(":8888", nil)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
 func main() {
